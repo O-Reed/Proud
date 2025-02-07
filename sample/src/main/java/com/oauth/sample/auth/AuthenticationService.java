ced5d75ca123dcbcba313c539e4cfb48be75aaf0
O-Reed <o.reed.super@gmail.com>
2025-02-07 00:13:44 -0500

    Authentication_JWT_authenticate

--- sample/src/main/java/com/oauth/sample/auth/AuthenticationService.java
+++ sample/src/main/java/com/oauth/sample/auth/AuthenticationService.java
@@ -6,6 +6,8 @@ import com.oauth.sample.model.Role;
 import com.oauth.sample.repository.UserRepository;
 import com.oauth.sample.service.JWTService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,7 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -36,6 +39,19 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = repository.findByUsername(request.getEamil())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
