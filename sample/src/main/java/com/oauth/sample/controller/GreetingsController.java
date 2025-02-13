755906dadaa8bf63d1beaaf460311330cc1c15d9
O-Reed <o.reed.super@gmail.com>
2025-02-12 23:46:36 -0500

    Testing_Swaggar

--- sample/src/main/java/com/oauth/sample/controller/GreetingsController.java
+++ sample/src/main/java/com/oauth/sample/controller/GreetingsController.java
@@ -1,13 +1,18 @@
 package com.oauth.sample.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/demo-controller")
+@Hidden
 public class GreetingsController {
 
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "hello";
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from secured endpoint");
     }
 }
